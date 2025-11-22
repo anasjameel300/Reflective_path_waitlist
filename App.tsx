ad04e01a908aa1f5fb323ee46a38687e6d2f8a77
anasjameel300 <anasjameel300@gmail.com>
2025-11-23 01:21:06 +0530

    refactor: update Vercel Analytics import path from `/next` to `/react`

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { PhoneMockup } from './components/PhoneMockup';
 import { LandingForm } from './components/LandingForm';
 import { HomeView } from './components/HomeView';
